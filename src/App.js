d6f58361d593a7d0aa281f7365bcba776e93af83
“sanny085” <sannykumar085@gmail.com>
2022-10-24 18:39:41 +0530

    removed extra import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Counter from "./components/Counter";
-import { PersonDetails } from "./components/PersonDetails";
+// import Counter from "./components/Counter";
+// import { PersonDetails } from "./components/PersonDetails";
 import HeadTailGame from "./components/HeadTailGame";
 
 function App() {
